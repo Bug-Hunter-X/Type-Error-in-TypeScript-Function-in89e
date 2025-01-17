b7beef850c9994cc2341c4967e82d8b342c84c04
BugHunter-X <askzenai@gmail.com>
2025-01-17 03:42:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function addSafe(a: number | string, b: number | string): number {
+  const numA = typeof a === 'string' ? parseFloat(a) : a;
+  const numB = typeof b === 'string' ? parseFloat(b) : b;
+  return numA + numB; 
+}
+
+let result = addSafe(1, "2"); // Type safe now
+console.log(result); // Output 3
